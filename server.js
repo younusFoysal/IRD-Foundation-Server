c787193c2eb4366c754418558069c876cbea8f1b
younusFoysal <younus.faysal@gmail.com>
2025-01-06 03:40:09 +0600

    [+] Updated: NextJS local port added

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const corsOptions = {
     origin: [
         'http://localhost:5173',
         'http://localhost:5174',
+        'http://localhost:3000',
+        'http://localhost:3001',
         'https://cors-test.codehappy.dev',
     ],
     credentials: true,
